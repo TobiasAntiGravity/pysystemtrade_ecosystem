deb7efb42b2072a45d032136a1e24390598756e7
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2022-07-01 14:35:46 +0200

    Removed files that interfere with private_pysystemtrade download
    Removed commands.sh and mail_credentials_test.py as they interfere with the download of the private_pysystemtrade git repo

--- pysystemtrade/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# send email test
-python /opt/projects/pysystemtrade/private/mail_credentials_test.py
-
-# keep the container running after test of mail is done
-tail -f /dev/null
--- pysystemtrade/mail_credentials_test.py
+++
@@ -1,9 +0,0 @@
-from syslogdiag.emailing import send_mail_msg
-
-try:
-	send_mail_msg(body='This mail was sent to confirm that the mail credentials in private_config.yaml is correct. No worries - they were :)', 
-				  subject='test of mail login credentials')
-
-except:
-	pass
-    
