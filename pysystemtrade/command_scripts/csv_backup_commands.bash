4d2673738b7a46046cf428ce04e18119477fbdda
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2022-07-06 20:33:36 +0200

    Amended command scripts
    Amended so that processes run in parallell, not consequitvely. Must find a way to kill monitor when the rest is finished.

--- pysystemtrade/command_scripts/csv_backup_commands.bash
+++ pysystemtrade/command_scripts/csv_backup_commands.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # start the stack_handler
+
 cd sysproduction/linux/scripts
 backup_arctic_to_csv
 
-python /opt/projects/pysystemtrade/syscontrol.monitor.py
+
 
--- pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
+++ pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# start the stack_handler
 cd sysproduction/linux/scripts
-run_stack_handler
-run_capital_update
+run_stack_handler &
+run_capital_update &
+python /opt/projects/pysystemtrade/syscontrol.monitor.py &
 
-python /opt/projects/pysystemtrade/syscontrol.monitor.py
 
