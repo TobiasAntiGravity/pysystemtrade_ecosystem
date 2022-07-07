1435bf63f2768d9df0063bad4d99a91b9f84c982
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2022-07-07 09:32:30 +0200

    added startup to command scripts
    Added startup to stack_and_capital_handler and to daily_processes

--- pysystemtrade/command_scripts/daily_processes_commands.bash
+++ pysystemtrade/command_scripts/daily_processes_commands.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cd sysproduction/linux/scripts
-#python /opt/projects/pysystemtrade/syscontrol/monitor.py &
-run_daily_price_updates 
+startup
+
+run_daily_price_updates
 run_systems
 run_strategy_order_generator
 run_cleaners
 run_reports
-
+#python /opt/projects/pysystemtrade/syscontrol/monitor.py &
 
--- pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
+++ pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd sysproduction/linux/scripts
+startup
+wait
+
 run_stack_handler &
 run_capital_update &
-python /opt/projects/pysystemtrade/syscontrol/monitor.py
+#python /opt/projects/pysystemtrade/syscontrol/monitor.py
 
 
