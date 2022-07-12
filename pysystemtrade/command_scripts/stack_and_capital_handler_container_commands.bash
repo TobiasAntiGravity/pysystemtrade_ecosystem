34efdd609c1058c95b2ce9c05d7c83f8fc0d29d7
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2022-07-12 22:12:48 +0200

    added startup to stack handler command

--- pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
+++ pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 run_monitor_once.py
+#python3 run_monitor_once.py
 
 cd sysproduction/linux/scripts
+startup
+
 run_stack_handler &
 run_capital_update &
 python3 /opt/projects/pysystemtrade/syscontrol/monitor.py
