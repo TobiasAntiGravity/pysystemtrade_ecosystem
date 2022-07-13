d03d9b0a984687cd507f359674bc5b6eade6558b
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2022-07-13 11:49:19 +0200

    Remove continous monitor - add monitor_once
    After adding all control_config default value to the same file

--- pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
+++ pysystemtrade/command_scripts/stack_and_capital_handler_container_commands.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#python3 run_monitor_once.py
+python3 run_monitor_once.py
 
 cd sysproduction/linux/scripts
 startup
 
 run_stack_handler &
-run_capital_update &
-python3 /opt/projects/pysystemtrade/syscontrol/monitor.py
+run_capital_update
+#python3 /opt/projects/pysystemtrade/syscontrol/monitor.py
 
 
