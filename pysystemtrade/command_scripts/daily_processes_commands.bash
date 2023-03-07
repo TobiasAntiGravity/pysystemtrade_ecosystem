8b0dd8cbedf6edca652ac632338865d4ec095ec6
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2023-03-07 11:17:01 +0100

    Amended daily price wrangling
    Moved fx and contract updates, and multiple / adjusted prices updates away from price_updates container and into daily_processes.

--- pysystemtrade/command_scripts/daily_processes_commands.bash
+++ pysystemtrade/command_scripts/daily_processes_commands.bash
@@ -2,7 +2,7 @@
 
 cd sysproduction/linux/scripts
 . run_daily_fx_and_contract_updates
-. run_daily_price_updates
+. run_daily_update_multiple_adjusted_prices
 . run_systems
 . run_strategy_order_generator
 . run_cleaners
