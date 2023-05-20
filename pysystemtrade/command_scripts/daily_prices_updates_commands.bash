312e360db33ee997dd7c7589271ca972bb8e140b
TobiasAntiGravity <77267841+TobiasAntiGravity@users.noreply.github.com>
2023-05-20 16:19:03 +0200

    moved run_daily_update_multiple_adjusted_prices from daily_prices_updates_commands to daily_processes_commands

--- pysystemtrade/command_scripts/daily_prices_updates_commands.bash
+++ pysystemtrade/command_scripts/daily_prices_updates_commands.bash
@@ -3,4 +3,3 @@
 cd sysproduction/linux/scripts
 . run_daily_fx_and_contract_updates
 . run_daily_price_updates
-. run_daily_update_multiple_adjusted_prices
--- pysystemtrade/command_scripts/daily_processes_commands.bash
+++ pysystemtrade/command_scripts/daily_processes_commands.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd sysproduction/linux/scripts
+. run_daily_update_multiple_adjusted_prices
 . run_systems
 . run_strategy_order_generator
 . run_cleaners
